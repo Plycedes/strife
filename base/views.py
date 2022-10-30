a3f6d1a8c56c3447d9ab4265c8844655aedc9315
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2022-10-30 11:51:05 +0530

    Templates upgrade

--- base/views.py
+++ base/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms =[
+    {'id':1, 'name':'Unity Scripting'},
+    {'id':2, 'name':'Angular Delvelopers'},
+    {'id':3, 'name':'Blender tips'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):    
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i    
+    return render(request, 'base/room.html', {'room': room})
