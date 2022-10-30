7954f59982b9289f4ea9e9d5c6070f0df1c08912
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2022-10-30 13:06:10 +0530

    database update

--- base/views.py
+++ base/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms =[
-    {'id':1, 'name':'Unity Scripting'},
-    {'id':2, 'name':'Angular Delvelopers'},
-    {'id':3, 'name':'Blender tips'},
-]
-
-def home(request):    
+def home(request):
+    rooms = Room.objects.all() 
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i    
+    room = Room.objects.get(id=pk)   
     return render(request, 'base/room.html', {'room': room})
