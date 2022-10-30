9a0851c31f0e8aa2738adca9a58921d5a687407a
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2022-10-30 18:07:40 +0530

    Sidebar backend

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
-def home(request):    
-    rooms = Room.objects.all() 
-    return render(request, 'base/home.html', {'rooms': rooms})
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''   
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+
+    context = {
+        'rooms': rooms,
+        'topics': topics
+    }
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)   
